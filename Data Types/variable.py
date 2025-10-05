98915ed7dd2e747af7d76b1fb43cbb39047e7259
Pradeep Ummiti <pradeepummiti@yahoo.com>
2025-10-04 20:54:53 -0400

    Create variable.py
    Programs to depict variable usages in python

---
+++ Data Types/variable.py
@@ -0,0 +1,59 @@
+# Program 1: Assigning an integer to a variable
+'''
+age = 30
+
+print(f"The age is: {age}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 2: Assigning a string to a variable
+'''
+name = "Alice"
+
+print(f"The name is: {name}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 3: Assigning a float to a variable
+'''
+price = 19.99
+
+print(f"The price is: {price}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 4: Assigning a boolean to a variable
+'''
+is_active = True
+
+print(f"The boolean value is: {is_active}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 5: Use type() function to get the type of objects
+'''
+integer_variable = 10
+float_variable = 3.14
+string_variable = "Hello"
+list_variable = [1, 2, 3]
+dictionary_variable = {"a": 1, "b": 2}
+
+print(type(integer_variable))
+print(type(float_variable))
+print(type(string_variable))
+print(type(list_variable))
+print(type(dictionary_variable))
+
+class MyClass:
+    pass
+
+my_object = MyClass()
+print(type(my_object))
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 6: Calculate taxes based on income
+'''
+my_income = 100
+tax_rate = 0.1
+
+my_tax = my_income * tax_rate
+
+print(f"I owe {my_tax} as tax")
+'''
+#---------------------------------------------------------------------------------------------------------
