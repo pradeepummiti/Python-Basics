87c6c3ba418f8aaf6b5aa72c061a113bb4c02c30
Pradeep Ummiti <pradeepummiti@yahoo.com>
2025-12-22 11:56:22 -0500

    Create booleans.py

---
+++ Data Types/booleans.py
@@ -0,0 +1,16 @@
+# Program 1: Check the data type of booleans
+'''
+print(f"The data type for Boolean True is: {type(True)}")
+print(f"The data type for Boolean False is: {type(False)}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 2: Check for comparison between 2 numbers
+'''
+print(f"Is 1 > 2: {1 > 2}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 3: Check for comparison between 2 numbers
+'''
+print(f"Is 1 == 1: {1 == 1}")
+'''
+#---------------------------------------------------------------------------------------------------------
