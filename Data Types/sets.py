20383ddc4a273fceffcf581d9ebe05fecc4e1e96
Pradeep Ummiti <pradeepummiti@yahoo.com>
2025-12-22 09:22:44 -0500

    Create sets.py

---
+++ Data Types/sets.py
@@ -0,0 +1,31 @@
+# Program 1: Add objects to a set
+'''
+my_set = set()
+print(f"The initial set is: {my_set}")
+my_set.add(1)
+print(f"The modified set is: {my_set}")
+my_set.add(2)
+print(f"The modified set is: {my_set}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 2: Add same object mulitple times in a set
+'''
+my_set = set()
+print(f"The initial set is: {my_set}")
+my_set.add(1)
+print(f"The modified set is: {my_set}")
+my_set.add(1)
+print(f"The modified set is: {my_set}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 3: Display numbers just once using a set
+'''
+#The below list has numbers appearing more than once
+my_list = [1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4]
+
+print(f"The initial list is: {my_list}")
+
+#The below set ensures the numbers do not repeat
+print(f"The new set is: {set(my_list)}")
+'''
+#---------------------------------------------------------------------------------------------------------
