7a32ae89f15cc11dd63a2b84492789455cc66c69
Pradeep Ummiti <pradeepummiti@yahoo.com>
2025-12-22 09:02:47 -0500

    Create tuple.py

---
+++ Data Types/tuple.py
@@ -0,0 +1,56 @@
+# Program 1: Determine the data type of a list and a tuple 
+'''
+my_list = ['Alice', 100, 5.7]
+my_tuple = (1, 2, 3)
+
+print(f"The data type of the list is: {type(my_list)}")
+print(f"The data type of the tuple is: {type(my_tuple)}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 2: Determine the length of a list and a tuple 
+'''
+my_list = ['Alice', 100, 5.7]
+my_tuple = (1, 2, 3)
+
+print(f"The data type of the list is: {len(my_list)}")
+print(f"The data type of the tuple is: {len(my_tuple)}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 3: Modify an existing object/item from a tuple 
+'''
+my_tuple = (1, 2, 3)
+
+print(f"The original tuple is: {my_tuple}")
+my_tuple = ('one', 'two', 3)
+print(f"The modified tuple is: {my_tuple}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 4: Count the number of occurances an object appears in a tuple 
+'''
+my_tuple = ('a', 'a', 'b', 'c')
+
+print(f"The original tuple is: {my_tuple}")
+print(f"The number of times 'a' appears in the tuple is: {my_tuple.count('a')}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 5: Display the index an object in a tuple 
+'''
+my_tuple = ('a', 'a', 'b', 'c')
+
+print(f"The original tuple is: {my_tuple}")
+print(f"The index of times 'a' in the tuple is: {my_tuple.index('a')}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 6: Demsontrate item assignment in a list and a tuple 
+'''
+my_list = ['Alice', 100, 5.7]
+my_tuple = (1, 2, 3)
+
+print(f"The original list is: {my_list}")
+my_list[0] = 'Wonderland'
+my_tuple[0] = 'Wonderland'
+print(f"The updated list is: {my_list}")
+print(f"The original tuple is: {my_tuple}")
+print(f"The updated tuple is: {my_tuple}")
+'''
+#---------------------------------------------------------------------------------------------------------
