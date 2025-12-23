6b98ffbf573a79f95cd363109c3a6f43c834c46c
Pradeep Ummiti <pradeepummiti@yahoo.com>
2025-12-23 11:14:39 -0500

    Create basic_comp_oper.py

---
+++ Comparison Operators/basic_comp_oper.py
@@ -0,0 +1,81 @@
+# Program 1: Check for equality between two numbers
+'''
+num1 = 10
+num2 = 25
+
+# Check for equality
+print(f"Are the numbers {num1} and {num2} equal?: {num1 == num2}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 2: Check for equality between two strings
+'''
+my_string1 = "Bye"
+my_string2 = "bye"
+
+# Check for equality
+print(f"Are the strings {my_string1} and {my_string2} equal?: {my_string1 == my_string2}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 3: Check for equality between a string and a number
+'''
+my_string1 = "2"
+num1 = 2
+
+# Check for equality
+print(f"Is the string {my_string1} and number {num1} equal?: {my_string1 == num1}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 4: Check for equality between a float and an integer number
+'''
+float1 = 2.0
+int1 = 2
+
+# Check for equality
+print(f"Is the floating-point number {float1} and integer number {int1} equal?: {float1 == int1}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 5: Check for non-equality between two numbers
+'''
+num1 = 3.0
+num2 = 3
+
+# Check for equality
+print(f"Are the numbers {num1} and {num2} equal?: {num1 != num2}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 6: Check for greater number between two numbers
+'''
+num1 = 4
+num2 = 1
+
+# Check for greater number
+print(f"Is {num1} greater than {num2}?: {num1 > num2}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 7: Check for lesser number between two numbers
+'''
+num1 = 4
+num2 = 1
+
+# Check for lesser number
+print(f"Is {num1} greater than {num2}?: {num1 < num2}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 8: Check for greater than or equal to a number between two numbers
+'''
+num1 = 2
+num2 = 2
+
+# Check for greater than or euqal to
+print(f"Is {num1} greater than {num2}?: {num1 >= num2}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 9: Check for lesser than or equal to a number between two numbers
+'''
+num1 = 2
+num2 = 2
+
+# Check for lesser than or euqal to
+print(f"Is {num1} lesser than {num2}?: {num1 <= num2}")
+'''
+#---------------------------------------------------------------------------------------------------------
