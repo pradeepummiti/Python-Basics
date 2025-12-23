e71b2508eb5ed535a99677e42ba056ac1c4230b2
Pradeep Ummiti <pradeepummiti@yahoo.com>
2025-12-23 16:11:01 -0500

    Create if_else_statements.py

---
+++ Statements/if_else_statements.py
@@ -0,0 +1,24 @@
+# Program 1: Illustrate if and else conditions
+'''
+place = "Bank"
+
+# Check the place and validate
+if (place == "Showroom"):
+  print(f"I'm at the showroom")
+else:
+  print(f"I'm not at the showroom")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 2: Illustrate if, else-if and else conditions
+'''
+place = "Bank"
+
+# Check the place and validate
+if (place == "Showroom"):
+  print(f"I'm at the showroom")
+elif (place == "Bank"):
+  print(f"I'm at the bank")
+else:
+  print(f"I'm neither at the showroom nor at the bank")
+'''
+#---------------------------------------------------------------------------------------------------------
