8f25d6cbd1e841880ec4c64188ece11f055c6c39
Pradeep Ummiti <pradeepummiti@yahoo.com>
2025-12-23 17:06:48 -0500

    Create while_loop.py

---
+++ Statements/while_loop.py
@@ -0,0 +1,62 @@
+# Program 1: Display the numbers from 0 to 4 using while loop
+'''
+num = 0
+
+# Check the value of temp variable if it is less than 5
+while (num < 5):
+  print(f"The value is: {num}")
+  num = num + 1
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 2: Display the numbers from 0 to 4 using while loop and else condition
+'''
+num = 0
+
+# Check the value of temp variable if it is less than 5
+while (num < 5):
+  print(f"The value is: {num}")
+  num += 1
+else:
+  print(f"The value is not less than 5 anymore")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 3: Demonstrate a "pass" keyword
+'''
+my_list = [0, 1, 2, 3]
+
+for item in my_list:
+  pass
+print(f"This is the end of my code")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 4: Demonstrate a "continue" keyword
+'''
+my_string = "Hello"
+
+for item in my_string:
+  if item == "e":
+    continue
+  print(f"{item}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 5: Demonstrate a "break" keyword
+'''
+my_string = "Hello"
+
+for item in my_string:
+  if item == "e":
+    break
+  print(f"{item}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 6: Demonstrate a "break" keyword
+'''
+num = 0
+
+while (num < 5):
+  if num == 2:
+    break
+  print(f"{num}")
+  num += 1
+'''
+#---------------------------------------------------------------------------------------------------------
