f0582782ceacfd9f9744754359214284d22469b4
Pradeep Ummiti <pradeepummiti@yahoo.com>
2025-12-23 15:26:33 -0500

    Create basic_logical_oper.py

---
+++ Comparison Operators/basic_logical_oper.py
@@ -0,0 +1,50 @@
+# Program 1: Compare more than two numbers in a single comparison statement
+'''
+
+# Check for equality
+print(f"What is the boolean result of 1 < 2 < 3?: {1 < 2 < 3}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 2: Compare more than two numbers in a single comparison statement
+'''
+
+# Check for equality
+print(f"What is the boolean result of 1 < 2 > 3?: {1 < 2 > 3}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 3: Compare more than two numbers in a single comparison statement using logical operator
+'''
+
+# Check for equality
+print(f"What is the boolean result of 1 < 2 > 3?: {(1 < 2) and (2 > 3)}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 4: Comparison using logical operator
+'''
+
+# Check for equality
+print(f"What is the boolean result?: {("H" == "h") and (2.0 == 2)}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 5: Comparison using logical operator
+'''
+
+# Check for equality
+print(f"What is the boolean result?: {("h" == "h") and (2.0 == 2)}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 6: Comparison using logical operator
+'''
+
+# Check for equality
+print(f"What is the boolean result?: {("H" == "h") or (2.0 == 2)}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 7: Comparison using logical operator
+'''
+
+# Check for equality
+print(f"What is the boolean result?: {(1 == 1)}")
+print(f"What is the boolean result?: {not(1 == 1)}")
+'''
+#---------------------------------------------------------------------------------------------------------
