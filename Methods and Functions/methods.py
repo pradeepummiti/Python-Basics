5d528583c51265edbddbd56bed67fecc9d799b6f
Pradeep Ummiti <pradeepummiti@yahoo.com>
2025-12-25 15:44:03 -0500

    Create methods.py

---
+++ Methods and Functions/methods.py
@@ -0,0 +1,30 @@
+# Program 1: Append a number to existing list of numbers using append method
+'''
+my_list = [1, 2, 3, 4]
+
+# Append a new number to the list
+my_list.append(5)
+
+# Display the sum
+print(f"The updated list is: {my_list}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 2: Pop out a number from existing list of numbers using pop method
+'''
+my_list = [1, 2, 3, 4]
+
+# Pop the number from the list
+my_list.pop()
+
+# Display the sum
+print(f"The updated list is: {my_list}")
+'''
+#---------------------------------------------------------------------------------------------------------
+# Program 3: Display the python version
+'''
+from platform import python_version
+
+# Display the python version
+print(f"The python version is: {python_version()}")
+'''
+#---------------------------------------------------------------------------------------------------------
